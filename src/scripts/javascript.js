2acc33233aa449c7dc8101713bf8c9db8f305b7c
Takuto Shiozawa <t.salt1104@gmail.com>
2021-02-05 00:58:34 +0900

    Play music itsself (not via iTunes)

--- src/scripts/javascript.js
+++ src/scripts/javascript.js
@@ -1,5 +1,3 @@
-const execSync = require('child_process').execSync;
-const path =  require('path');
 const NodeID3 = require('node-id3');
 const { AsyncNedb } = require('nedb-async');
 
@@ -23,76 +21,108 @@ class Playlist {
   }
 }
 
-const startEl = document.getElementById('start');
-const stopEl = document.getElementById('stop');
 const autoEl = document.getElementById('auto');
 const registerEl = document.getElementById('register');
 const colorEl = document.getElementById('color');
 const titleEl = document.getElementById('title');
 const artistEl = document.getElementById('artist');
-const timeEl = document.getElementById('time');
+const durationEl = document.getElementById('duration');
 const positionEl = document.getElementById('position');
 const progressEl = document.getElementById('progress');
+const prevEl = document.getElementById('prev');
+const playEl = document.getElementById('play');
+const nextEl = document.getElementById('next');
+const audioEl = document.getElementById('audio');
 const lyricsEl = document.getElementById('lyrics');
 const updateMusicEl = document.getElementById('update-music');
 const searchEl = document.getElementById('search-song');
 const keywordEl = document.getElementById('keyword');
 
-//* 再生中の曲名・アーティスト名・再生時間
-let trackTitle = '';
-let trackArtist = '';
-let trackTime = 300;
+//* 曲の長さ
+let duration = 300;
+//* 歌詞の配列
 let lyricsArray = [];
-//* Interval処理をする変数。clearするために外部で設定
-let geterId;
-let trackerId;
-let counterId;
 //* 再生時間
-let playbackTime = 0;
-//* iTunesから取得した再生時間
-let nowPosition = 0;
-//* 再生時刻の取得時刻
-let countUpStart;
+let currentTime = 0;
+//* 再生リスト
+let playArray = [];
+//* 再生リスト中の現在位置
+let playingIndex = 0;
+//* 再生状態
+let playerState = 'stop';
 //* タイムテーブルの時間表（配列）
 let timeArray = [];
+//* 登録中の時間表
 let registeringArray = [];
 //* タイムテーブル登録中？
 let isRegistering = false;
-//* アラート表示させるフラグ
-let alertOn = true;
-//* オートスクロールさせる?
+//* オートスクロールさせる？
 let isAuto = true;
 //* フォントカラー
 let fontColor = 'red';
 const colorOption = ['red', 'blue', 'green', 'white'];
 
 /**
- * タイムテーブル登録でEnterを押すと時間が登録されるイベント
+ * ウィンドウ読み込み時に登録するキーイベント
  * @param {KeyboardEvent} event 
  */
-const pressKeyEvent = function(event) {
+function defaultKeyEvent(event) {
+  switch (event.code) {
+    case 'ArrowUp':
+    case 'ArrowDown':
+      event.preventDefault();
+      break;
+    case 'ArrowLeft':
+      event.preventDefault();
+      backTrack();
+      break;
+    case 'ArrowRight':
+      event.preventDefault();
+      nextTrack();
+      break;
+    case 'Space':
+      event.preventDefault();
+      togglePlay();
+      break;
+    default:
+      break;
+  }
+}
+
+/**
+ * タイムテーブル登録時にEnterを押すと時間が登録されるイベント
+ * @param {KeyboardEvent} event 
+ */
+async function registeringKeyEvent(event) {
   switch (event.code) {
     //* Enterボタン押下でタイムテーブルに追加
     case 'Enter':
-      const time = Math.floor(playbackTime * 100) / 100;
+      const time = Math.floor(currentTime * 100) / 100;
       registeringArray.push(time);
       break;
 
     //* 「←」「↑」ボタン押下で一つ戻す
     case 'ArrowLeft':
     case 'ArrowUp':
+      event.preventDefault()
       registeringArray.pop();
       break;
 
     //* 再登録の場合「↓」「→」ボタン押下で元をコピー
     case 'ArrowDown':
     case 'ArrowRight':
+      event.preventDefault()
       if (timeArray.length) {
         const i = registeringArray.length;
         registeringArray.push(timeArray[i]);
       }
       break;
 
+    case 'Space':
+      event.preventDefault();
+      togglePlay();
+      break;
+  
     default:
       break;
   }
@@ -102,9 +132,9 @@ const pressKeyEvent = function(event) {
     const res = confirm('タイムテーブルを登録しますか？');
     if (res) {
       //* タイムテーブルを保存
-      registerTimeTable();
+      await registerTimeTable();
       //* 保存したら最初から再生
-      repositionToBeginning();
+      audioEl.currentTime = 0;
       //* 登録したものをそのまま用いる
       timeArray = [...registeringArray];
       scrollTo(0, 0);
@@ -116,148 +146,41 @@ const pressKeyEvent = function(event) {
   }
 }
 
-/**
- * 定期的にiTunesから再生時間などの情報を取得する
- *    曲が変更されたら新たに歌詞を取得する
- */
-function repeatGetPosition() {
-  geterId = setInterval(function() {
-    const res = getPlayingPosition();
-    if (!res) return;
-
-    const { title, artist, time, position } = res;
-    //* 曲が変わった場合、フロントを書き換え
-    if (title !== trackTitle || artist !== trackArtist) {
-      alertOn = true;
-      countUpStart = new Date();
-      nowPosition = position;
-      setTrackInfo(title, artist, time);
-      //* タイムテーブル登録中に曲が変わった場合登録中止
-      if (isRegistering) {
-        quitRegistering();
-      }
-      isRegistering = false;
-    }
-
-    //* 取得する時間は正確に刻んでおらず、ブレてしまうので、
-    //* 秒数が取得時間よりも大きく（0.75秒程度）ずれていたら変更。
-    if (Math.abs(playbackTime - position) > 0.75) {
-      countUpStart = new Date();
-      nowPosition = position;
-    }
-  }, 2000);
-}
-
 /** 再生箇所の歌詞を強調表示させる */
 function trackLyrics() {
-  trackerId = setInterval(function() {
-    const liElements = lyricsEl.children;
-    const elRows = liElements.length;
-    const times = isRegistering ? registeringArray : timeArray;
-    for (let i = 0; i < elRows; i++) {
-      if (times.length) {
-        //* タイムテーブルを参照し、再生時間よりも小さいものを強調表示
-        liElements[i].className = times[i] && times[i] < playbackTime ? `passed ${fontColor}` : '';
-      } else {
-        //* タイムテーブルがない場合、経過時間と曲の長さで大体のところまで
-        liElements[i].className = (i < (elRows * (playbackTime / trackTime))) ? 'passed' : '';
-      }
+  const liElements = lyricsEl.children;
+  const elRows = liElements.length;
+  const times = isRegistering ? registeringArray : timeArray;
+  for (let i = 0; i < elRows; i++) {
+    if (times.length || isRegistering) {
+      //* タイムテーブルを参照し、再生時間よりも小さいものを強調表示
+      liElements[i].className = times[i] && times[i] < currentTime ? `passed ${fontColor}` : '';
+    } else {
+      //* タイムテーブルがない場合、経過時間と曲の長さで大体のところまで
+      liElements[i].className = (i < (elRows * (currentTime / duration))) ? 'passed none' : '';
     }
+  }
 
-    //* 自動スクロールがONならば
-    if (isAuto) {
-      scrollToLyrics();
-    }
-  }, 100);
-}
-
-/** 連続して再生時間の更新させる */
-function countUpTime() {
-  counterId = setInterval(function() {
-    //* iTunesから再生位置を取得した時間からの経過時間で計算
-    const nowTime = new Date();
-    const elapsedTime = ((nowTime.getTime() - countUpStart.getTime()) / 1000);
-    playbackTime = nowPosition + elapsedTime;
-    //* プログレスバーを更新
-    progressEl.value = Math.floor(playbackTime);
-    positionEl.textContent = Math.floor(playbackTime / 60) + ':' + ('0' + Math.floor(playbackTime % 60)).slice(-2);
-  }, 100);
-}
-
-/**
- * 再生中の曲情報（曲名、アーティスト名）と現在の再生時間を取得するメソッド
- * @return {{ title: string, artist: string, time: string, position: number }}
- *   曲名, アーティスト名, 再生時間
- */
-function getPlayingPosition() {
-  const iTunesOperatePath = path.join(__dirname, 'scripts', 'iTunesOperation.scpt');
-  const script = 'osascript ' + iTunesOperatePath + ' "playing"';
-  const currentTrack = runAndDecode(script);
-  return currentTrack;
-}
-
-/**
- * タイムテーブルが存在しないときに再生中の歌詞を取得するメソッド
- * @return {string[]} 歌詞の配列
- */
-function getOriginalLyrics() {
-  const iTunesOperatePath = path.join(__dirname, 'scripts', 'iTunesOperation.scpt');
-  const script = 'osascript ' + iTunesOperatePath + ' "lyrics"';
-  const lyrics = runAndDecode(script);
-  return lyrics;
-}
-
-/** 曲を一番最初から始めるメソッド */
-function repositionToBeginning() {
-  const iTunesOperatePath = path.join(__dirname, 'scripts', 'iTunesOperation.scpt');
-  const script = 'osascript ' + iTunesOperatePath + ' "back"';
-  execSync(script);
-  //* 時間もリセット
-  playbackTime = 0;
-  nowPosition = 0;
-  countUpStart = new Date();
-}
-
-function jumpPlayPosition(position) {
-  const iTunesOperatePath = path.join(__dirname, 'scripts', 'iTunesOperation.scpt');
-  const script = 'osascript ' + iTunesOperatePath + ' "jump" ' + position;
-  execSync(script);
-  playbackTime = position;
-  nowPosition = position;
-  countUpStart = new Date();
-}
-
-/**
- * 再生している曲のタイムテーブルを配列で取得するメソッド
- * @return {{ time: number, lyrics: string }[]} 
- *   歌詞とその表示時刻の配列
- */
-function getTimeTable() {
-  const findLyricsPath = path.join(__dirname, 'scripts', 'FinderOperation.scpt');
-  const script = `osascript ${findLyricsPath} "get" "${trackTitle}" "${trackArtist}"`;
-  const timeList = runAndDecode(script);
-  if (!timeList) return;
-  
-  const timeRegex = /\[?([0-9\.]+)\] /;
-  const timeTable = timeList.filter(n => n !== '').map(text => {
-    const time = Number(timeRegex.exec(text)[1]);
-    const lyrics = text.split('] ')[1];
-    return { time, lyrics };
-  });
-  return timeTable;
+  //* 自動スクロールがONならば
+  if (isAuto) {
+    scrollToLyrics();
+  }
 }
 
 /** タイムテーブルを作成するメソッド */
-function registerTimeTable() {
-  const findLyricsPath = path.join(__dirname, 'scripts', 'FinderOperation.scpt');
-  let script = `osascript ${findLyricsPath} "create" "${trackTitle}" "${trackArtist}"`;
-
-  //* シェルスクリプトの引数にタイムテーブルを追加していく
+async function registerTimeTable() {
+  const timeTable = [];
   for (let i = 0; i < registeringArray.length; i++) {
-    script += ` "[${registeringArray[i]}] ${lyricsArray[i]}"`;
+    const content = `[${registeringArray[i]}] ${lyricsArray[i]}`;
+    timeTable.push(content);
   }
 
-  execSync(script);
+  const { title, artist } = playArray[playingIndex];
+  const songDB = new AsyncNedb({
+    filename: 'src/db/songs.db',
+    autoload: true,
+  });
+  await songDB.asyncUpdate({ title, artist }, { $set: { timeTable } }, { multi: true });
 }
 
 /** タイムテーブル作成を中止するメソッド */
@@ -266,60 +189,35 @@ function quitRegistering() {
   registerEl.textContent = timeArray.length ? '再登録' : '登録';
   lyricsEl.classList.remove('registering');
   //* Enterボタン押下でタイム記録するイベント削除
-  document.removeEventListener('keydown', pressKeyEvent);
+  window.removeEventListener('keydown', registeringKeyEvent);
   registeringArray = [];
 }
 
-/**
- * applescriptを実行してその戻り値をデコード・パース処理をしてJSONを返すメソッド
- * @param {string} script スクリプト実行コード
- * @return {JSON} JSONオブジェクト
- */
-function runAndDecode(script) {
-  const response = execSync(script);
-  const decode = new TextDecoder().decode(response);
-  if (!decode) return '';
-  try {
-    const json = JSON.parse(decode);
-    return json;
-  } catch (err) {
-    if (alertOn) {
-      alert(decode);
-    }
-    alertOn = false;
-    return '';
-  }
-}
+/** 曲が更新された時に歌詞（タイムテーブル）を取得 */
+function getAndSetTrackInfo() {
+  const { title, artist, path, timeTable } = playArray[playingIndex];
+  audioEl.src = path;
+  audioEl.play();
 
-/**
- * 曲が更新された時にフロントの曲情報を書き換え
- * @param {string} title 曲名
- * @param {string} artist アーティスト名
- * @param {string} time 再生時間
- */
-function setTrackInfo(title, artist, time) {
-  trackTitle = title;
-  trackArtist = artist;
-  //* 曲の再生時間を算出
-  const splitTime = time.split(':');
-  trackTime = Number(splitTime[0]) * 60 + Number(splitTime[1]);
-  progressEl.max = trackTime;
-  //* 表示更新
-  titleEl.textContent = trackTitle;
-  artistEl.textContent = trackArtist;
-  timeEl.textContent = time;
-
-  const timeTable = getTimeTable();
+  titleEl.textContent = title;
+  artistEl.textContent = artist;
+  
   if (timeTable) {
+    const timeRegex = /\[?([0-9\.]+)\] /;
+    const timetables = timeTable.filter(n => n !== '').map(text => {
+      const time = Number(timeRegex.exec(text)[1]);
+      const lyrics = text.split('] ')[1];
+      return { time, lyrics };
+    });
     //* タイムテーブルが存在する時、色替え機能を開始
     //* インターバル処理多重起動の防止のため、再起動
-    timeArray = timeTable.map(n => n.time);
-    lyricsArray = timeTable.map(n => n.lyrics);
+    timeArray = timetables.map(n => n.time);
+    lyricsArray = timetables.map(n => n.lyrics);
     registerEl.textContent = '再登録';
   } else {
     timeArray = [];
-    //* iTunesから歌詞を取得。戻り値の配列が先頭空白なので消去
-    lyricsArray = getOriginalLyrics().slice(1);
+    //* ID3タグから歌詞を取得
+    lyricsArray = getLyricsFromPath(path);
     registerEl.textContent = '登録';
   }
   //* 自動スクロールの開始
@@ -332,6 +230,12 @@ function setTrackInfo(title, artist, time) {
     lyricsHtml += `<li>${lyr}</li>`;
   });
   lyricsEl.innerHTML = lyricsHtml;
+}
+
+function getLyricsFromPath(path) {
+  const id3Tag = NodeID3.read(path);
+  const lyrics = id3Tag.unsynchronisedLyrics && id3Tag.unsynchronisedLyrics.text;
+  return lyrics ? lyrics.split(/\r\n|\n|\r/) : [];
 }
 
 /** 現在の歌詞の位置へスクロール */
@@ -358,7 +262,7 @@ async function updateSongList(event) {
   const musicPath = '/Users/shiozawatakuto/Music/iTunes/iTunes Media/';
   //* 曲情報データベースを取得
   const db = new AsyncNedb({
-    filename: 'src/db/musics.db',
+    filename: 'src/db/songs.db',
     autoload: true,
   });  
   //* 返ってきた曲を配列で取得し、それぞれ処理
@@ -390,7 +294,7 @@ async function searchSongs() {
   //* キーワードから曲を検索
   //* 曲情報データベースを取得
   const songDB = new AsyncNedb({
-    filename: 'src/db/musics.db',
+    filename: 'src/db/songs.db',
     autoload: true,
   });
 
@@ -409,7 +313,6 @@ async function searchSongs() {
   if (!songs.length) return;
 
   const songIds = songs.map(song => song._id);
-  console.log(songIds);
 
   //* 検索された曲を含むプレイリストを検索
   //* プレイリストデータベースを取得
@@ -423,8 +326,50 @@ async function searchSongs() {
   console.log(playlists);
 }
 
+/**
+ * 秒数を時刻（MM:SS）にフォーマットするメソッド
+ * @param {number} time 時間（秒）
+ * @return {string} 時刻（MM:SS）
+ */
+function convertTime(time) {
+  return Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2);
+}
+
+/** 再生状態をスイッチするメソッド */
+function togglePlay() {
+  //* 停止中の場合、「再生」
+  if (playerState === 'pause') {
+    audioEl.play();
+  }
+  //* 再生中の場合、「停止」
+  if (playerState === 'playing') {
+    audioEl.pause();
+  }
+}
+
+/** 前曲へ */
+function backTrack() {
+  if (playerState === 'stop' || !playArray.length) return;
+  if (audioEl.currentTime > 3) {
+    audioEl.currentTime = 0;
+    return;
+  }
+  playingIndex = Math.max(playingIndex - 1, 0);
+  getAndSetTrackInfo();
+}
+
+/** 次曲へ */
+function nextTrack() {
+  if (playerState === 'stop' || !playArray.length) return;
+  playingIndex =
+    playArray.length === playingIndex + 1
+      ? 0
+      : playingIndex + 1;
+  getAndSetTrackInfo();
+}
+
 //* アプリ読み込み時に
-window.onload = () => {
+window.onload = async () => {
   //* フォントカラーの選択オプションを追加
   colorOption.forEach(color => {
     const option = document.createElement('option');
@@ -435,42 +380,9 @@ window.onload = () => {
     colorEl.appendChild(option);
   });
 
-  //* 上下キー入力でのスクロールを抑制
-  window.addEventListener('keydown', (event) => {
-    switch (event.code) {
-      case 'ArrowUp':
-      case 'ArrowDown':
-        event.preventDefault()
-        break;
-      default:
-        break;
-    }
-  });
+  //* デフォルトのキーイベントを設定
+  window.addEventListener('keydown', defaultKeyEvent);
 };
-
-//* 歌詞取得開始ボタンクリック時
-startEl.addEventListener('click', () => {
-  startEl.blur();
-  if (timeArray.length) {
-    changeLyricsColor();
-  }
-  repeatGetPosition();
-  trackLyrics();
-  countUpStart = new Date();
-  countUpTime();
-  startEl.className = 'display-none';
-  stopEl.className = 'display';
-});
-
-//* 停止ボタンクリック時
-stopEl.addEventListener('click', () => {
-  stopEl.blur();
-  clearInterval(geterId);
-  clearInterval(trackerId);
-  clearInterval(counterId);
-  startEl.className = 'display';
-  stopEl.className = 'display-none';
-});
 
 //* ユーザーがスクロールした時、自動スクロールを停止
 document.addEventListener('wheel', () => {
@@ -495,9 +407,11 @@ registerEl.addEventListener('click', () => {
     registerEl.textContent = '登録中止';
     lyricsEl.classList.add('registering');
     //* 曲を最初から再生し始める
-    repositionToBeginning();
+    audioEl.currentTime = 0;
+    //* デフォルトのキーイベントを削除
+    window.removeEventListener('keydown', defaultKeyEvent);
     //* Enterボタン押下でタイム記録するイベント追加
-    document.addEventListener('keydown', pressKeyEvent);
+    window.addEventListener('keydown', registeringKeyEvent);
   }
   isRegistering = !isRegistering;
 });
@@ -511,27 +425,18 @@ colorEl.addEventListener('change', (event) => {
 
 //* プログレスバーを操作中
 progressEl.addEventListener('input', (event) => {
-  //* 機能を一時停止
-  clearInterval(geterId);
-  clearInterval(changerId);
-  clearInterval(counterId);
-  //* プログレスバーの現在位置を更新
+  if (playerState === 'playing') {
+    audioEl.pause();
+  }
   const time = event.target.value;
-  positionEl.textContent = Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2);
+  positionEl.textContent = convertTime(time);
+  audioEl.currentTime = time;
 });
 
 //* プログレスバー操作終了時
-progressEl.addEventListener('change', (event) => {
-  //* 停止していた機能の再起動
-  //* タイムテーブル登録時も歌詞色替え機能再開
-  if (timeArray.length || registeringArray.length) {
-    changeLyricsColor();
-  }
-  repeatGetPosition();
-  countUpStart = new Date();
-  countUpTime();
-  //* 決定した再生時間にジャンプさせる
-  jumpPlayPosition(Number(event.target.value));
+progressEl.addEventListener('change', () => {
+  audioEl.play();
+  progressEl.blur();
 });
 
 //* 曲情報をアップデート
@@ -539,3 +444,63 @@ updateMusicEl.addEventListener('input', updateSongList);
 
 //* 曲の検索
 searchEl.addEventListener('click', searchSongs);
+
+//* 再生/停止ボタンクリック
+playEl.addEventListener('click', async () => {
+  //* 再生リストが空の時
+  if (!playArray.length) {
+    const songDB = new AsyncNedb({
+      filename: 'src/db/songs.db',
+      autoload: true,
+    });
+    //* 適当に曲を取ってきて再生リストに格納・再生
+    playArray = await songDB.asyncFind({ title: /[abc]/ });
+    getAndSetTrackInfo();
+  }
+
+  //* 再生状態トグル
+  togglePlay();
+  playEl.blur();
+});
+
+//* 「前へ」ボタン
+prevEl.addEventListener('click', () => {
+  backTrack();
+  prevEl.blur();
+});
+
+//* 「次へ」ボタン
+nextEl.addEventListener('click', () => {
+  nextTrack();
+  nextEl.blur();
+});
+
+//* 一時停止した時
+audioEl.addEventListener('pause', () => {
+  playerState = 'pause';
+  playEl.textContent = '再生';
+});
+
+//* 再生した時
+audioEl.addEventListener('play', () => {
+  playerState = 'playing';
+  playEl.textContent = '停止';
+});
+
+//* 曲が最後まで行った時
+audioEl.addEventListener('ended', nextTrack);
+
+//* 曲が読み込まれた時、再生時間を更新
+audioEl.addEventListener('loadeddata', () => {
+  duration = audioEl.duration;
+  durationEl.textContent = convertTime(duration);
+  progressEl.max = Math.floor(duration);
+});
+
+//* 現在の再生位置が変更された時
+audioEl.addEventListener('timeupdate', () => {
+  currentTime = audioEl.currentTime;
+  progressEl.value = currentTime;
+  positionEl.textContent = convertTime(currentTime);
+  trackLyrics();
+});
